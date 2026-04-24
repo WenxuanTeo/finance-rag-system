5e2394f4eb5f0a8cb3b3a48e01970fe8413f06db
WenxuanTeo <zwxchina666@163.com>
2026-04-24 16:52:36 +0800

    Update chunk.py

--- spark/chunk.py
+++ spark/chunk.py
@@ -1,1 +1,8 @@
-
+def chunk_data(data):
+    chunks = []
+    for item in data:
+        chunks.append({
+            "id": item["id"],
+            "chunk": item["text"]
+        })
+    return chunks
