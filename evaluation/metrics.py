1602e16f6192d61e65f141d6a34dff6444e9bb07
WenxuanTeo <zwxchina666@163.com>
2026-04-24 16:53:17 +0800

    Update metrics.py

--- evaluation/metrics.py
+++ evaluation/metrics.py
@@ -1,1 +1,6 @@
-
+def hit_rate(results, target_keyword):
+    hit = 0
+    for score, item in results:
+        if target_keyword in item["chunk"]:
+            hit += 1
+    return hit / len(results)
