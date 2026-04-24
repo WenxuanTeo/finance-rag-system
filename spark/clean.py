746d4f5b71ea24fc66a657fea3b160f9d6521b64
WenxuanTeo <zwxchina666@163.com>
2026-04-24 16:51:48 +0800

    Update clean.py

--- spark/clean.py
+++ spark/clean.py
@@ -1,1 +1,6 @@
-
+def clean_data(data):
+    cleaned = []
+    for item in data:
+        text = item["text"].replace("，", " ")
+        cleaned.append({"id": item["id"], "text": text})
+    return cleaned
